0d598e44238a8dd46d4e9e30df03d6deace67fcf
Raghav Goswami <raghav.goswami576@gmail.com>
2021-02-20 18:38:23 -0500

    create components

---
+++ prj-start/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+import { Component } from "@angular/core";
+
+@Component({
+  // decorator allows angular to recognize this as a component
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
+})
+export class HeaderComponent {}
