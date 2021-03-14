b803cb7f494bac6cbe17a2f517193ca5d77b79a0
Raghav Goswami <raghav.goswami576@gmail.com>
2021-03-14 19:32:07 -0400

    close dropdown by clicking anywhere outside

--- prj-start/src/app/shared/dropdown.directive.ts
+++ prj-start/src/app/shared/dropdown.directive.ts
@@ -1,14 +1,29 @@
-import { Directive, HostListener, HostBinding } from "@angular/core";
+import {
+  Directive,
+  HostListener,
+  HostBinding,
+  ElementRef,
+} from "@angular/core";
 
 @Directive({
   selector: "[appDropdown]",
 })
 export class DropdownDirective {
   @HostBinding("class.open") isOpen: boolean = false; //bind to prop of element dir is placed on
-  //class is an array of all the classes
-  @HostListener("click") toggleOpen() {
-    //listen to event and execute method
-    this.isOpen = !this.isOpen;
+  // class is an array of all the classes
+  // below implementation opens and closes dropdown by clicking on it
+  //   @HostListener("click") toggleOpen() {
+  //     //listen to event and execute method
+  //     this.isOpen = !this.isOpen;
+  //   }
+
+  // close dropdown by clicking anywhere outside
+  @HostListener("document:click", ["$event"]) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? this
+      : !this;
   }
+
+  constructor(private elRef: ElementRef) {}
   // attaches open class to the element it sits on when it's clicked on
 }
